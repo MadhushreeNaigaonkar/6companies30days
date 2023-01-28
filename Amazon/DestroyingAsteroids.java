e67894c6d307badaab4fdb5079024f07e836b7d6
MadhushreeNaigaonkar <madhunai2951@gmail.com>
2023-01-28 13:19:23 +0530

    Day 2 Milestone 6 Amazon

---
+++ Amazon/DestroyingAsteroids.java
@@ -0,0 +1,19 @@
+package Amazon;
+
+import java.util.Arrays;
+
+class Solution {
+   
+        public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        long m = mass;
+        Arrays.sort(asteroids);
+        for (int ast : asteroids) {
+            if (m < ast) {
+                return false;
+            }
+            m += ast;
+        }
+        return true;
+    }
+    
+}
---
+++ Amazon/MaxSumHourglass.java
@@ -0,0 +1,18 @@
+package Amazon;
+
+class Solution {
+    public int maxSum(int[][] grid) {
+        int max = -1, m = grid.length, n = grid[0].length;
+        for (int row = 0; row < m - 2; row++) {
+            for (int col = 0; col < n - 2; col++) {
+                int temp = grid[row][col] + grid[row][col + 1] + grid[row][col + 2];
+                temp += grid[row + 1][col + 1];
+                temp += grid[row + 2][col] + grid[row + 2][col + 1] + grid[row + 2][col + 2];
+                if (max < temp) {
+                    max = temp;
+                }
+            }
+        }
+        return max;
+    }
+}
